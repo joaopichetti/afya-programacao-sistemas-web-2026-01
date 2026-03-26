06118f543318eb9ea71805f33368715bd05deee8
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-03-25 23:49:37 -0300

    Projeto criado para testar Live Preview

---
+++ aula_04/teste-live-preview/script.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-teste').addEventListener('click', () => {
+  const mensagem = document.getElementById('mensagem');
+  mensagem.textContent = 'Parabéns! JavaScript está funcionando! 🎉';
+});
