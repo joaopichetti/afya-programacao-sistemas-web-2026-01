626252a6bc70218ab45c11b58941a0d929aaac92
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-04-13 22:36:18 -0300

    Aula JS: implementação da lista de tarefas

--- aula-08/exemplo-js/script.js
+++ aula-08/exemplo-js/script.js
@@ -47,3 +47,80 @@ function calcularResultado(valor1, valor2, operacao) {
 
     return resultado;
 }
+
+// --- LÓGICA DO TODO LIST ---
+let tarefas = [];
+let indiceTarefaSelecionada = -1;
+
+const inputTarefa = document.querySelector('#tarefa');
+const btnSalvar = document.querySelector('#btnSalvar');
+btnSalvar.addEventListener('click', salvarTarefa);
+const listaTarefas = document.querySelector('#listaTarefas');
+
+function renderizarTarefas() {
+    listaTarefas.innerHTML = '';
+    for (let indice = 0; indice < tarefas.length; indice++) {
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        const btnRemover = document.createElement('button');
+
+        span.textContent = tarefas[indice];
+        btnRemover.textContent = 'Remover';
+        btnRemover.classList.add('btn-remover');
+        btnRemover.addEventListener('click', 
+            () => removerTarefa(indice));
+
+        li.appendChild(span);
+        li.appendChild(btnRemover);
+        li.addEventListener('click', 
+            () => carregarTarefaParaEdicao(indice));
+
+        listaTarefas.appendChild(li);
+    }
+}
+
+function salvarTarefa() {
+    const text = inputTarefa.value.trim();
+    if (text === '') return;
+
+    if (indiceTarefaSelecionada < 0) {
+        tarefas.push(text);
+        alert('Tarefa criada com sucesso!');
+    } else {
+        tarefas[indiceTarefaSelecionada] = text;
+        alert('Tarefa atualizada com sucesso!');
+        indiceTarefaSelecionada = -1;
+        btnSalvar.textContent = 'Adicionar';
+    }
+
+    inputTarefa.value = '';
+    inputTarefa.focus();
+    renderizarTarefas();
+}
+
+function carregarTarefaParaEdicao(indice) {
+    console.log('Iniciou a edição da tarefa');
+    inputTarefa.value = tarefas[indice];
+    inputTarefa.focus();
+    indiceTarefaSelecionada = indice;
+    btnSalvar.textContent = 'Atualizar';
+}
+
+function removerTarefa(indice) {
+    console.log('Iniciou a remoção da tarefa');
+    event.stopPropagation();
+
+    const confirmacao = confirm('Deseja realmente excluir esta tarefa?');
+    if (confirmacao) {
+        tarefas.splice(indice, 1);
+        renderizarTarefas();
+        alert('Tarefa excluída com sucesso');
+
+        if (indiceTarefaSelecionada === indice) {
+            indiceTarefaSelecionada = -1;
+            btnSalvar.textContent = 'Adicionar';
+            inputTarefa.value = '';
+            inputTarefa.focus();
+        }
+    }
+}
