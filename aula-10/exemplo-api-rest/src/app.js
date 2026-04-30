43b929fe042bd7c987abf19ab95f504a84906cb1
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-04-29 21:31:35 -0300

    Aula APIs: Criação de métodos para listar e criar alunos

--- aula-10/exemplo-api-rest/src/app.js
+++ aula-10/exemplo-api-rest/src/app.js
@@ -6,6 +6,9 @@ const app = express();
 // Constante para a porta padrão
 const PORT = 3000;
 
+// Middlewares
+app.use(express.json());
+
 // Rotas (Endpoints)
 app.get('/', (req, res) => {
     const paginaHtml = `
@@ -21,7 +24,69 @@ app.get('/', (req, res) => {
     res.send(paginaHtml);
 });
 
+// 1. GET: Retorna todos os alunos
+app.get('/alunos', (req, res) => {
+    res.json(alunos);
+});
+
+// 2. GET: (por ID): Retorna um aluno específico
+app.get('/alunos/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const aluno = alunos.find(aluno => aluno.id === id);
+
+    if (!aluno) {
+        // 404 Not Found
+        return res.status(404).json({ erro: "Aluno não encontrado." });
+    }
+
+    res.json(aluno);
+});
+
+// 3. POST: Cria um novo aluno
+app.post('/alunos', (req, res) => {
+    const { nome, idade, mensalidade, ativo, linguagens } = req.body;
+
+    // Validação básica (400 Bad Request)
+    if (!nome || !idade || ativo === undefined) {
+        return res.status(400).json({
+            erro: "Nome, idade, e ativo são campos obrigatórios"
+        });
+    }
+
+    const novoAluno = new Aluno(
+        proximoId++,
+        nome,
+        idade,
+        mensalidade,
+        ativo,
+        linguagens,
+    );
+
+    alunos.push(novoAluno);
+
+    // 201 Created
+    res.status(201).json(novoAluno);
+});
+
 // Inicialização do Servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando em: http://localhost:${PORT}`);
 });
+
+// Modelo (Classe Aluno)
+class Aluno {
+    constructor(id, nome, idade, mensalidade, ativo, linguagens) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+        this.mensalidade = mensalidade;
+        this.ativo = ativo;
+        this.linguagens = linguagens;
+        this.dataDeCadastro = new Date().toISOString();
+    }
+}
+
+// Banco de dados em memória (simulação)
+let alunos = [];
+// Variável para controlar o ID autoincremento
+let proximoId = 1;
