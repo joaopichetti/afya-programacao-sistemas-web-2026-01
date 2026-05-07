dd6a43a8cebd05a5e25220ad5142bf944003f66c
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2026-05-06 21:49:13 -0300

    Aula React: Implementadas as funcionalidades de edição e exclusão

--- aula-11/exemplo-react/src/App.jsx
+++ aula-11/exemplo-react/src/App.jsx
@@ -1,18 +1,53 @@
 import { useState } from "react";
+import './App.css';
 
 function App() {
   // Estado para armazenar a lista em memória
   const [alunos, setAlunos] = useState([]);
   const [nome, setNome] = useState('');
+  const [alunoEmEdicao, setAlunoEmEdicao] = useState(null);
 
   const salvarAluno = (event) => {
     event.preventDefault();
-    const novoAluno = {
-      id: Date.now(),
+
+    if (!nome.trim()) {
+      window.alert('Informe o nome do aluno');
+      return;
+    }
+
+    const alunoParaSalvar = {
+      id: alunoEmEdicao ? alunoEmEdicao.id : Date.now(),
       nome: nome,
     };
-    setAlunos([...alunos, novoAluno]);
+
+    if (alunoEmEdicao) {
+      setAlunos(alunos.map(aluno =>
+        aluno.id === alunoParaSalvar.id 
+          ? alunoParaSalvar : aluno
+      ));
+      setAlunoEmEdicao(null);
+    } else {
+      setAlunos([...alunos, alunoParaSalvar]);
+    }
     setNome('');
+  };
+
+  const carregarAlunoParaEditar = (aluno) => {
+    setAlunoEmEdicao(aluno);
+    setNome(aluno.nome);
+  };
+
+  const removerAluno = (event, id) => {
+    event.stopPropagation();
+    const confirm = window.confirm(
+      'Tem certeza que deseja remover este aluno?'
+    );
+    if (!confirm) return;
+    setAlunos(alunos.filter(aluno => aluno.id !== id));
+    if (alunoEmEdicao && alunoEmEdicao.id === id) {
+      setAlunoEmEdicao(null);
+      setNome('');
+    }
   };
 
   return (
@@ -22,11 +57,18 @@ function App() {
         <input type="text" placeholder="Nome do Aluno" value={nome}
             onChange={(event) => setNome(event.target.value)}
             required />
-        <button type="submit">Adicionar</button>
+        <button type="submit">
+          {alunoEmEdicao ? 'Atualizar' : 'Adicionar'}
+        </button>
       </form>
       <ul>
         {alunos.map((aluno) => (
-          <li key={aluno.id}>{aluno.id} - {aluno.nome}</li>
+          <li key={aluno.id} onClick={() => carregarAlunoParaEditar(aluno)}>
+            <span>{aluno.id} - {aluno.nome}</span>
+            <button onClick={(event) => removerAluno(event, aluno.id)}>
+              Remover
+            </button>
+          </li>
         ))}
       </ul>
     </div>
